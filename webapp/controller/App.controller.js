f52b3358eead6000a455eddd3f99c841d43bce22
YSCHREIER <yolanda.schreier@gmail.com>
2023-03-22 15:41:09 +0000

    COMMIT 04 ELEMENTOS Y CONTROLES UI5

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -10,6 +10,30 @@ sap.ui.define([
         return Controller.extend("employees.controller.App", {
             onInit: function () {
 
-            }
+            },
+
+            onLiveChangeInputEmployee: function (oEvent) {
+
+                console.log(oEvent.getParameters());
+
+                let sValue = oEvent.getParameter("newValue"),
+                    oInput = oEvent.getSource();
+
+                console.log(oInput);
+
+                if(sValue.length === oInput.getMaxLength()){
+                    oInput.setDescription("OK");
+                    this.getView().byId("lblCountry").setVisible(true);
+                    this.getView().byId("selectCountry").setVisible(true);
+                    
+                }
+                else{
+
+                    oInput.setDescription("not OK");
+                    this.getView().byId("lblCountry").setVisible(false);
+                    this.getView().byId("selectCountry").setVisible(false);
+                }
+
+            },
         });
     });
