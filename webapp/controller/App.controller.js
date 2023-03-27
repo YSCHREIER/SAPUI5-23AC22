bbc379146f522a0ae30849f46a46622a9d746fb4
YSCHREIER <yolanda.schreier@gmail.com>
2023-03-27 18:56:47 +0000

    PRIMER COMMIT

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -131,5 +131,115 @@ sap.ui.define([
 
             },
 
+            onShowOrders: function(oEvent)
+            {
+                let oOrderTable = this.getView().byId("OrderTable");
+
+                //LIMPIAR EL CONTENEDOR POR SI YA EXISTE
+                oOrderTable.destroyItems();
+
+                //OBTENER LAS ORDENES DEL EMPLEADO SELECCIONADO
+                let oBindingContext = oEvent.getSource().getBindingContext("jsonEmployees");
+                let oOrders = oBindingContext.getProperty("Orders");
+
+                //2 formas para obtener el ORDER
+                console.log(oBindingContext.getObject().Orders);
+                console.log(oOrders);
+
+                let aColumns = [],
+                    aItems = [];
+
+                //CREAMOS LAS COLUMNAS POR SEPARADO
+                let aColumnOrderId = new sap.m.Column({header: new sap.m.Label({text:"{i18n>OrderId}"})}),
+                    aColumnFreight = new sap.m.Column({header: new sap.m.Label({text:"{i18n>Freight}"})}),
+                    aColumnShipAddress = new sap.m.Column({header: new sap.m.Label({text:"{i18n>ShipAddress}"})});
+
+                //AGREGAMOS LAS COLUMNAS AL ARRAY
+                aColumns.push(aColumnOrderId,aColumnFreight,aColumnShipAddress);
+
+                //CREAMOS LOS ITEMS
+
+                oOrders.forEach((oOrder) => {
+                    let oTemplate = new sap.m.ColumnListItem({
+                        
+                        cells: [
+                                //new sap.m.ObjectIdentifier({title:oOrder.OrderID}),
+                                new sap.m.ObjectIdentifier({title: oOrder.OrderID}),
+                                new sap.m.Text({text: oOrder.Freight}),
+                                new sap.m.Text({text: oOrder.ShipAddress})                                
+                            ]
+                    })
+
+                    aItems.push(oTemplate);
+                });
+
+            //CREAMOS LA TABLA
+
+            let oTable = new sap.m.Table({
+                columns : aColumns,
+                items: aItems,
+                width : "auto"
+            }).addStyleClass("sapUiSmallMargin");
+
+            //oOrderTable.addItem(oTable);
+
+            //OTRA FORMA DE CREAR TABLA
+            let oNewTable = new sap.m.Table();
+            oNewTable.setWidth("auto");
+            oNewTable.addStyleClass("sapUiSmallMargin");
+
+            //CREAR LAS COLUMNAS
+
+            let oColumnOrderId2 = new sap.m.Column(),
+                oLabelOrderId2 = new sap.m.Label();
+
+                oLabelOrderId2.bindProperty("text","i18n>OrderId" );
+                oColumnOrderId2.setHeader(oLabelOrderId2);
+                oNewTable.addColumn(oColumnOrderId2);
+
+            
+            let oColumnFreight2 = new sap.m.Column(),
+                oLabelFreight2 = new sap.m.Label();
+
+                oLabelFreight2.bindProperty("text","i18n>Freight" );
+                oColumnFreight2.setHeader(oLabelFreight2);
+                oNewTable.addColumn(oColumnFreight2);
+
+            let oColumnShipAddress2 = new sap.m.Column(),
+                oLabelShipAddress2 = new sap.m.Label();
+
+                oLabelShipAddress2.bindProperty("text","i18n>ShipAddress" );
+                oColumnShipAddress2.setHeader(oLabelShipAddress2);
+                oNewTable.addColumn(oColumnShipAddress2);
+
+            let oTemplate2 = new sap.m.ColumnListItem();
+
+            let oCellOrderId = new sap.m.ObjectIdentifier();
+            oCellOrderId.bindProperty("title","jsonEmployees>OrderID" );
+            oTemplate2.addCell(oCellOrderId);
+
+            let oCellOFreight = new sap.m.Text();
+            oCellOFreight.bindProperty("text","jsonEmployees>Freight" );
+            oTemplate2.addCell(oCellOFreight);
+
+            let oCellShipAddress = new sap.m.Text();
+            oCellShipAddress.bindProperty("text","jsonEmployees>ShipAddress" );
+            oTemplate2.addCell(oCellShipAddress);
+
+            //CREAMOS EL BINDING
+
+            let oBinding = {
+                path : 'Orders',
+                model : 'jsonEmployees',
+                template : oTemplate2
+            };
+
+            oNewTable.bindAggregation("items", oBinding);
+            oNewTable.bindElement("jsonEmployees>" + oBindingContext.getPath());
+             
+            oOrderTable.addItem(oNewTable);
+
+            }
+
         });
     });
